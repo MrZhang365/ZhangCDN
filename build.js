17dee492e539ae24f7626b5299a6e74d4db5b5ea
MrZhang365 <MrZhang365@outlook.com>
2023-09-30 23:54:28 +0800

    fix a bug

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 const path = require('path')
 
-const packages = fs.readdirSync('./static').filter(d => fs.statSync(path.resolve(__dirname, 'static', d)).isFile())
+const packages = fs.readdirSync('./static').filter(d => !fs.statSync(path.resolve(__dirname, 'static', d)).isFile())
 fs.writeFileSync('packages.txt', packages.join('\n'))
